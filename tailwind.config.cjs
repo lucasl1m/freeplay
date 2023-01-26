c522b99e213704a70d808b7eb9e8629556732d0e
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-01-26 17:13:56 -0300

    feat: config tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#171B1F',
+        white: '#FFFFFF',
+
+        blue: {
+          500: '#1991EB',
+          700: '#21699F',
+        },
+
+        gray: {
+          500: '#8D8D8D',
+          800: '#252B33',
+        },
+      },
+
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
